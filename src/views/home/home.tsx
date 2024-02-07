72983b6316c30abe86cf00a4e4cdfe3d7d127bf0
mortenaho <mortenaho@gmail.com>
2024-02-07 20:56:32 +0330

    update home.css and home.tsx

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, GridColumn, GridRow, Segment } from 'semantic-ui-react';
+import { Container, Grid, GridColumn, GridRow, Segment } from 'semantic-ui-react';
 import GetProducts from '../../services/product-service';
 import Prodoct from '../../components/product';
 import IProduct from '../../Interfaces/iproduct';
- 
+import './home.css'
 
 function Home() {
     const [products, setProducts] = useState<IProduct[]>([]);
@@ -15,17 +15,22 @@ function Home() {
     }, []);
 
     return (
-        <Grid  columns={3} >
-            <GridRow  >
-                {products != null && products.map((prodcut, index) => {
-                    return (<GridColumn  className='mb-10'  tablet={6} computer={4} mobile={12}>
-                            <Prodoct data={prodcut}/>
-                    </GridColumn>)
-                })}
-            </GridRow>
-
-
-        </Grid>
+        <Container fluid>
+            <Grid>
+                <GridRow centered verticalAlign='middle' textAlign='center' color='blue' className='top-banner'>
+                    <h1>حراجی برای مهاجرت</h1>
+                </GridRow>
+            </Grid>
+            <Grid columns={3} padded='horizontally'  >
+                <GridRow  >
+                    {products != null && products.map((prodcut, index) => {
+                        return (<GridColumn className='mb-10' tablet={6} computer={4} mobile={12}>
+                            <Prodoct data={prodcut} />
+                        </GridColumn>)
+                    })}
+                </GridRow>
+            </Grid>
+        </Container>
     );
 }
 
